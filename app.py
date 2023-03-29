1033f72650386b37068ff23b7936f084302e1dc5
lakshmirajaperumal <34375861+lakshmirajaperumal@users.noreply.github.com>
2023-03-29 16:10:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
-import cv2
-import base64
+
 from flask import Flask, Response
 
 app = Flask(__name__)
 
-def generate_frames():
-    cap = cv2.VideoCapture(0)
 
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
